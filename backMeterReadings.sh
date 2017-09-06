7a9039901d46f6401c8637399ab6a9fcc99d9b8d
Clive Carrington <clive.carrington@btinternet.com>
2017-09-06 23:46:47 +0100

    Completed a simply two stage copy to a new table

--- backMeterReadings.sh
+++ backMeterReadings.sh
@@ -5,7 +5,12 @@
 
 # p = Ass1ngt0n
 
+# takes structure and data from meterReading in measurements and creates a 
+# SQL command file to recreate it.
+
 mysqldump --opt -u pi_manage -p  measurements meterReadings > ./backup/meterReadings.sql
 
+
+Takes the SQL command file and applies it to the backup database
 mysql -u pi_manage -p backupMeasurements < ./backup/meterReadings.sql
 
