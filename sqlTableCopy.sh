300cb7d533bc052ff5aa018adab9734ca9d228d6
Clive Carrington <clive.carrington@btinternet.com>
2017-09-13 00:39:25 +0100

    Added complete Table pull

--- sqlTableCopy.sh
+++ sqlTableCopy.sh
@@ -2,9 +2,12 @@
 
 # /bin/rm -f ./backup/testBackupScript.sql
 
+username="pi_manage"
+pw="Ass1ngt0n"
+
 if [ $# -lt 1 ]
 then
-        echo "Usage : $0 [pull_struct | pull_data | push_data | list"
+        echo "Usage : $0 [pull_struct | pull_data | pull_table | push_data | list"
         exit
 fi
 
@@ -15,7 +18,7 @@ pull_struct)  echo "Pull Structure only"
 	if [ -f ./.temp/$4.sql ] ; then
 		rm -f ./.temp/$4.sql
 	fi
-	mysqldump --opt --add-drop-table --no-data -u pi_manage -pAss1ngt0n  $2 $3 > ./.temp/$4.sql
+	mysqldump --opt --add-drop-table --no-data -u $username -p$pw  $2 $3 > ./.temp/$4.sql
 	echo "Now attempt to save it to len.carrington.mySQL"
 	s3cmd put ./.temp/$4.sql s3://len.carrington.mySQL/$4.sql
     ;;
@@ -23,7 +26,15 @@ pull_data)  echo  "Pull Data from existing table"
 	if [ -f ./.temp/$4.sql ] ; then
 		rm -f ./.temp/$4.sql
 	fi
-	mysqldump --opt --skip-add-drop-table --no-create-info  -u pi_manage -pAss1ngt0n $2 $3 > ./.temp/$4.sql
+	mysqldump --opt --skip-add-drop-table --no-create-info  -u $username -p$pw $2 $3 > ./.temp/$4.sql
+	echo "Now attempt to save it to len.carrington.mySQL"
+	s3cmd put ./.temp/$4.sql s3://len.carrington.mySQL/$4.sql
+    ;;  
+pull_table)  echo  "Pull Data from existing table"
+	if [ -f ./.temp/$4.sql ] ; then
+		rm -f ./.temp/$4.sql
+	fi
+	mysqldump --opt -u $username -p$pw $2 $3 > ./.temp/$4.sql
 	echo "Now attempt to save it to len.carrington.mySQL"
 	s3cmd put ./.temp/$4.sql s3://len.carrington.mySQL/$4.sql
     ;;  
@@ -34,13 +45,13 @@ push_data)  echo  "Push Saved  SQL file"
 	fi
 	echo "Attempt to retrieve it from len.carrington.mySQL"
 	s3cmd get s3://len.carrington.mySQL/$3.sql ./.temp/$3.sql
-	mysql -u pi_manage -pAss1ngt0n $2 < ./.temp/$3.sql
+	mysql -u $username -p$pw $2 < ./.temp/$3.sql
 	echo
 	echo "list of the new copy database"
 	echo
-	mysql -u pi_manage -pAss1ngt0n testCopyDatabase < ./listRecords.sql
+	mysql -u $username -p$pw $2 < ./listRecords.sql
     ;;
-list)	echo "The following fies are avialable in the s3 bucket"
+list)	echo "The following files are available in the s3 bucket"
 	s3cmd ls s3://len.carrington.mySQL
     ;;
 *) 	echo "The command $1 is not known"
